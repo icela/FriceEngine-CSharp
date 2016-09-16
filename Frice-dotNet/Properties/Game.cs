038e12c1bc30411bf81a51341911381676605fee
ice1000 <ice1000kotlin@foxmail.com>
2016-09-16 19:50:22 +0800

    initial commit

---
+++ Frice-dotNet/Properties/Game.cs
@@ -0,0 +1,33 @@
+﻿using System.Threading;
+using System.Windows.Forms;
+
+namespace Frice_dotNet.FriceEngine
+{
+	public class Game : Form
+	{
+		public Game()
+		{
+			SetBounds(100, 100, 500, 500);
+			OnInit();
+			Show();
+			new Thread(Run).Start();
+		}
+
+		public void OnInit()
+		{
+		}
+
+		public void OnRefresh()
+		{
+		}
+
+		private void Run()
+		{
+			while (true)
+			{
+				OnRefresh();
+			}
+			// ReSharper disable once FunctionNeverReturns
+		}
+	}
+}
