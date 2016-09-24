65a6a66540886cc3107ab98eda6279b8e8231d13
ifdog <zouyuanpeng@163.com>
2016-09-24 20:39:45 +0800

    按我的理解，应该是这么个结构？

---
+++ FriceEngine/Utils/Time/FTimer.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace FriceEngine.Utils.Time
+{
+    public class FTimer
+    {
+        protected int Time;
+
+        public int Times { get; protected set; }
+
+        public int Start = DateTime.Now.Millisecond;
+
+        public FTimer(int time, int times)
+        {
+            Time = time;
+            Times = times;
+        }
+
+        public FTimer(int time)
+        {
+            Time = time;
+            Times = -1;
+        }
+
+        public bool Ended()
+        {
+            if (DateTime.Now.Millisecond - Start <= Time || Times == 0) return false;
+            Start = DateTime.Now.Millisecond;
+            if (Times > 0) Times--;
+            return true;
+        }
+    }
+
+    public class FTimeListener:FTimer
+    {
+        public FTimeListener(int time, int times) : base(time, times)
+        {
+        }
+
+        public FTimeListener(int time) : base(time)
+        {
+        }
+
+        public void Check()
+        {
+            if (!Ended() || Times == 0) return;
+            if (Times > 0) --Times;
+            // TODO invoke the event
+        }
+    }
+}
---
+++ FriceEngineTest/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FriceEngine.Animation;
+using FriceEngine.Utils.Graphics;
+
+namespace FriceEngineTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            new Test();
+        }
+    }
+
+    public class Test : FriceEngine.Game
+    {
+        
+        public override void OnInit()
+        {
+            var a = new FriceEngine.Object.ShapeObject(FriceEngine.Resource.ColorResource.吾王蓝,new FCircle(40),100,100 );
+            a.MoveList.Add(new SimpleMove(5,2));
+            AddObject(a);
+     
+        }
+
+    }
+}
