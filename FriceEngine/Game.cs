6d5623ad3a1e5d1f601809d8b51b739982c6e2de
ice1000 <ice1000kotlin@foxmail.com>
2016-09-26 11:57:35 +0800

    code style

--- FriceEngine/Game.cs
+++ FriceEngine/Game.cs
@@ -10,214 +10,217 @@ using FriceEngine.Utils.Time;
 
 namespace FriceEngine
 {
-    public class AbstractGame : Panel
-    {
-        internal readonly IList<IAbstractObject> Objects;
-        internal readonly IList<IAbstractObject> ObjectAddBuffer;
-        internal readonly IList<IAbstractObject> ObjectDeleteBuffer;
+	public class AbstractGame : Panel
+	{
+		internal readonly IList<IAbstractObject> Objects;
+		internal readonly IList<IAbstractObject> ObjectAddBuffer;
+		internal readonly IList<IAbstractObject> ObjectDeleteBuffer;
 
-        internal readonly IList<FText> Texts;
-        internal readonly IList<FText> TextAddBuffer;
-        internal readonly IList<FText> TextDeleteBuffer;
+		internal readonly IList<FText> Texts;
+		internal readonly IList<FText> TextAddBuffer;
+		internal readonly IList<FText> TextDeleteBuffer;
 
-        internal readonly IList<FTimeListener> FTimeListeners;
-        internal readonly IList<FTimeListener> FTimeListenerAddBuffer;
-        internal readonly IList<FTimeListener> FTimeListenerDeleteBuffer;
+		internal readonly IList<FTimeListener> FTimeListeners;
+		internal readonly IList<FTimeListener> FTimeListenerAddBuffer;
+		internal readonly IList<FTimeListener> FTimeListenerDeleteBuffer;
 
-        internal AbstractGame()
-        {
-            Objects = new List<IAbstractObject>();
-            ObjectAddBuffer = new List<IAbstractObject>();
-            ObjectDeleteBuffer = new List<IAbstractObject>();
+		internal AbstractGame()
+		{
+			Objects = new List<IAbstractObject>();
+			ObjectAddBuffer = new List<IAbstractObject>();
+			ObjectDeleteBuffer = new List<IAbstractObject>();
 
-            Texts = new List<FText>();
-            TextAddBuffer = new List<FText>();
-            TextDeleteBuffer = new List<FText>();
+			Texts = new List<FText>();
+			TextAddBuffer = new List<FText>();
+			TextDeleteBuffer = new List<FText>();
 
-            FTimeListeners = new List<FTimeListener>();
-            FTimeListenerAddBuffer = new List<FTimeListener>();
-            FTimeListenerDeleteBuffer = new List<FTimeListener>();
-        }
+			FTimeListeners = new List<FTimeListener>();
+			FTimeListenerAddBuffer = new List<FTimeListener>();
+			FTimeListenerDeleteBuffer = new List<FTimeListener>();
+		}
 
-        protected override void OnPaint(PaintEventArgs e)
-        {
-            ProcessBuffer();
-            foreach (var l in FTimeListeners) l.Check();
-            foreach (var o in Objects) (o as FObject)?.HandleAnims();
+		protected override void OnPaint(PaintEventArgs e)
+		{
+			ProcessBuffer();
+			foreach (var l in FTimeListeners) l.Check();
+			foreach (var o in Objects) (o as FObject)?.HandleAnims();
 
-            var g = e.Graphics;
-            g.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.AntiAlias;
-            g.CompositingQuality = System.Drawing.Drawing2D.CompositingQuality.HighQuality;
-            g.TextRenderingHint = System.Drawing.Text.TextRenderingHint.AntiAliasGridFit;
-            foreach (var o in Objects)
-                if (o is ShapeObject)
-                {
-                    var brush = new SolidBrush((o as ShapeObject).ColorResource.Color);
-                    if ((o as ShapeObject).Shape is FRectangle)
-                        g.FillRectangle(brush,
-                            (float) o.X,
-                            (float) o.Y,
-                            (float) (o as ShapeObject).Width,
-                            (float) (o as ShapeObject).Height
-                        );
-                    else if ((o as ShapeObject).Shape is FOval)
-                        g.FillEllipse(brush,
-                            (float) o.X,
-                            (float) o.Y,
-                            (float) (o as ShapeObject).Width,
-                            (float) (o as ShapeObject).Height
-                        );
-                }
-                else if (o is ImageObject)
-                {
-                    g.DrawImage((o as ImageObject).Bmp, (o as ImageObject).Point);
-                }
-            foreach (var t in Texts)
-            {
-            }
-            base.OnPaint(e);
-        }
+			var g = e.Graphics;
+			g.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.AntiAlias;
+			g.CompositingQuality = System.Drawing.Drawing2D.CompositingQuality.HighQuality;
+			g.TextRenderingHint = System.Drawing.Text.TextRenderingHint.AntiAliasGridFit;
+			foreach (var o in Objects)
+				if (o is ShapeObject)
+				{
+					var brush = new SolidBrush((o as ShapeObject).ColorResource.Color);
+					if ((o as ShapeObject).Shape is FRectangle)
+						g.FillRectangle(brush,
+							(float) o.X,
+							(float) o.Y,
+							(float) (o as ShapeObject).Width,
+							(float) (o as ShapeObject).Height
+						);
+					else if ((o as ShapeObject).Shape is FOval)
+						g.FillEllipse(brush,
+							(float) o.X,
+							(float) o.Y,
+							(float) (o as ShapeObject).Width,
+							(float) (o as ShapeObject).Height
+						);
+				}
+				else if (o is ImageObject)
+				{
+					g.DrawImage((o as ImageObject).Bmp, (o as ImageObject).Point);
+				}
+			foreach (var t in Texts)
+			{
+				var brush = new SolidBrush(t.GetColor().Color);
+				if (t is SimpleText)
+					g.DrawString(t.Text, DefaultFont, brush, (float) t.X, (float) t.Y);
+			}
+			base.OnPaint(e);
+		}
 
-        private void ProcessBuffer()
-        {
-            foreach (var o in ObjectAddBuffer) Objects.Add(o);
-            foreach (var o in ObjectDeleteBuffer) Objects.Remove(o);
-            ObjectAddBuffer.Clear();
-            ObjectDeleteBuffer.Clear();
+		private void ProcessBuffer()
+		{
+			foreach (var o in ObjectAddBuffer) Objects.Add(o);
+			foreach (var o in ObjectDeleteBuffer) Objects.Remove(o);
+			ObjectAddBuffer.Clear();
+			ObjectDeleteBuffer.Clear();
 
-            foreach (var t in TextAddBuffer) Texts.Add(t);
-            foreach (var t in TextDeleteBuffer) Texts.Remove(t);
-            TextAddBuffer.Clear();
-            TextDeleteBuffer.Clear();
+			foreach (var t in TextAddBuffer) Texts.Add(t);
+			foreach (var t in TextDeleteBuffer) Texts.Remove(t);
+			TextAddBuffer.Clear();
+			TextDeleteBuffer.Clear();
 
-            foreach (var t in FTimeListenerAddBuffer) FTimeListeners.Add(t);
-            foreach (var t in FTimeListenerDeleteBuffer) FTimeListeners.Remove(t);
-            FTimeListenerAddBuffer.Clear();
-            FTimeListenerDeleteBuffer.Clear();
-        }
-    }
+			foreach (var t in FTimeListenerAddBuffer) FTimeListeners.Add(t);
+			foreach (var t in FTimeListenerDeleteBuffer) FTimeListeners.Remove(t);
+			FTimeListenerAddBuffer.Clear();
+			FTimeListenerDeleteBuffer.Clear();
+		}
+	}
 
-    public class Game : Form
-    {
-        public Game()
-        {
-            SetBounds(100, 100, 500, 500);
-            FormBorderStyle = FormBorderStyle.FixedSingle;
-            DoubleBuffered = true;
-            MaximizeBox = false;
+	public class Game : Form
+	{
+		public Game()
+		{
+			SetBounds(100, 100, 500, 500);
+			FormBorderStyle = FormBorderStyle.FixedSingle;
+			DoubleBuffered = true;
+			MaximizeBox = false;
 
-            Icon = (System.Drawing.Icon) new ComponentResourceManager(typeof(Icon)).GetObject("icon");
+			Icon = (System.Drawing.Icon) new ComponentResourceManager(typeof(Icon)).GetObject("icon");
 
-            _syncContext = SynchronizationContext.Current;
-            _gamePanel = new AbstractGame();
-            OnInit();
-            _gamePanel.SetBounds(0, 0, Width, Height);
-            Controls.Add(_gamePanel);
-            Show();
-            Run();
-            // ReSharper disable VirtualMemberCallInConstructor
-        }
+			_syncContext = SynchronizationContext.Current;
+			_gamePanel = new AbstractGame();
+			OnInit();
+			_gamePanel.SetBounds(0, 0, Width, Height);
+			Controls.Add(_gamePanel);
+			Show();
+			Run();
+			// ReSharper disable VirtualMemberCallInConstructor
+		}
 
-        private readonly SynchronizationContext _syncContext;
-        private readonly AbstractGame _gamePanel;
+		private readonly SynchronizationContext _syncContext;
+		private readonly AbstractGame _gamePanel;
 
 //        private readonly Graphics _gameScene;
 //        private readonly Bitmap _screenCut;
 
-        public new Point MousePosition() => Control.MousePosition;
+		public new Point MousePosition() => Control.MousePosition;
 
-        /// <summary>
-        /// a java-like setTitle method.
-        /// set the window title.
-        /// </summary>
-        /// <param name="title">text of the title</param>
-        public void SetTitle(string title)
-        {
-            Text = title;
-        }
+		/// <summary>
+		/// a java-like setTitle method.
+		/// set the window title.
+		/// </summary>
+		/// <param name="title">text of the title</param>
+		public void SetTitle(string title)
+		{
+			Text = title;
+		}
 
-        /// <summary>
-        /// hide the cursor.
-        /// </summary>
-        public void HideCursor() => Cursor.Hide();
+		/// <summary>
+		/// hide the cursor.
+		/// </summary>
+		public void HideCursor() => Cursor.Hide();
 
-        /// <summary>
-        /// add an object or text to screen.
-        /// </summary>
-        /// <param name="o">the object or text to be added.</param>
-        public void AddObject(IAbstractObject o)
-        {
-            if (o == null) return;
-            if (o is FText) _gamePanel.TextAddBuffer.Add((FText) o);
-            else _gamePanel.ObjectAddBuffer.Add(o);
-        }
+		/// <summary>
+		/// add an object or text to screen.
+		/// </summary>
+		/// <param name="o">the object or text to be added.</param>
+		public void AddObject(IAbstractObject o)
+		{
+			if (o == null) return;
+			if (o is FText) _gamePanel.TextAddBuffer.Add((FText) o);
+			else _gamePanel.ObjectAddBuffer.Add(o);
+		}
 
-        /// <summary>
-        /// remove an object or text from screen.
-        /// </summary>
-        /// <param name="o">the object or text to be removed.</param>
-        public void RemoveObject(IAbstractObject o)
-        {
-            if (o == null) return;
-            if (o is FText) _gamePanel.TextDeleteBuffer.Add((FText) o);
-            else _gamePanel.ObjectDeleteBuffer.Add(o);
-        }
+		/// <summary>
+		/// remove an object or text from screen.
+		/// </summary>
+		/// <param name="o">the object or text to be removed.</param>
+		public void RemoveObject(IAbstractObject o)
+		{
+			if (o == null) return;
+			if (o is FText) _gamePanel.TextDeleteBuffer.Add((FText) o);
+			else _gamePanel.ObjectDeleteBuffer.Add(o);
+		}
 
-        /// <summary>
-        /// clear all objects and texts
-        /// </summary>
-        public void ClearObjects()
-        {
-            foreach (var o in _gamePanel.Objects) _gamePanel.ObjectDeleteBuffer.Add(o);
-            foreach (var o in _gamePanel.Texts) _gamePanel.TextDeleteBuffer.Add(o);
-        }
+		/// <summary>
+		/// clear all objects and texts
+		/// </summary>
+		public void ClearObjects()
+		{
+			foreach (var o in _gamePanel.Objects) _gamePanel.ObjectDeleteBuffer.Add(o);
+			foreach (var o in _gamePanel.Texts) _gamePanel.TextDeleteBuffer.Add(o);
+		}
 
-        /// <summary>
-        /// add a timerListener
-        /// </summary>
-        /// <param name="t">the timeListener to be added.</param>
-        public void AddTimeListener(FTimeListener t) => _gamePanel.FTimeListenerAddBuffer.Add(t);
+		/// <summary>
+		/// add a timerListener
+		/// </summary>
+		/// <param name="t">the timeListener to be added.</param>
+		public void AddTimeListener(FTimeListener t) => _gamePanel.FTimeListenerAddBuffer.Add(t);
 
-        /// <summary>
-        /// remove a timeListener
-        /// </summary>
-        /// <param name="t">the timeListener to be removed.</param>
-        public void RemoveTimeListener(FTimeListener t) => _gamePanel.FTimeListenerDeleteBuffer.Add(t);
+		/// <summary>
+		/// remove a timeListener
+		/// </summary>
+		/// <param name="t">the timeListener to be removed.</param>
+		public void RemoveTimeListener(FTimeListener t) => _gamePanel.FTimeListenerDeleteBuffer.Add(t);
 
-        /// <summary>
-        /// clear the timeListeners.
-        /// </summary>
-        public void ClearTimeListeners()
-        {
-            foreach (var l in _gamePanel.FTimeListeners) _gamePanel.FTimeListenerDeleteBuffer.Add(l);
-        }
+		/// <summary>
+		/// clear the timeListeners.
+		/// </summary>
+		public void ClearTimeListeners()
+		{
+			foreach (var l in _gamePanel.FTimeListeners) _gamePanel.FTimeListenerDeleteBuffer.Add(l);
+		}
 
-        public virtual void OnInit()
-        {
-        }
+		public virtual void OnInit()
+		{
+		}
 
-        public virtual void OnRefresh()
-        {
-        }
+		public virtual void OnRefresh()
+		{
+		}
 
-        public virtual void OnClick()
-        {
-        }
+		public virtual void OnClick()
+		{
+		}
 
-        /// <summary>
-        /// 感谢ifdog老司机帮我修改这个问题。。。
-        /// </summary>
-        private void Run()
-        {
-            var fTimer2 = new FTimer2(50);
-            fTimer2.Start(() =>
-            {
-                _syncContext.Send(state =>
-                {
-                    OnRefresh();
-                    _gamePanel.Refresh();
-                }, null);
-            });
-        }
-    }
+		/// <summary>
+		/// 感谢ifdog老司机帮我修改这个问题。。。
+		/// </summary>
+		private void Run()
+		{
+			var fTimer2 = new FTimer2(50);
+			fTimer2.Start(() =>
+			{
+				_syncContext.Send(state =>
+				{
+					OnRefresh();
+					_gamePanel.Refresh();
+				}, null);
+			});
+		}
+	}
 }
--- FriceEngineTest/Program.cs
+++ FriceEngineTest/Program.cs
@@ -9,40 +9,40 @@ using FriceEngine.Utils.Graphics;
 
 namespace FriceEngineTest
 {
-    public static class Program
-    {
-        public static void Main(string[] args)
-        {
-            // ReSharper disable once ObjectCreationAsStatement
-            Application.Run(new Test());
-        }
-    }
+	public static class Program
+	{
+		public static void Main(string[] args)
+		{
+			// ReSharper disable once ObjectCreationAsStatement
+			Application.Run(new Test());
+		}
+	}
 
-    public class Test : Game
-    {
-        public override void OnInit()
-        {
-            Width = 800;
-            Height = 600;
+	public class Test : Game
+	{
+		public override void OnInit()
+		{
+			Width = 800;
+			Height = 600;
 
-            SetTitle("Fuck the world");
+			SetTitle("Fuck the world");
 
-            var a = new ShapeObject(ColorResource.吾王蓝, new FCircle(40), 400, 300);
-            //replace with a file path in desk
-            var b = ImageObject.FromFile(@"C:\frice.png", 300, 400, 50, 50);
+			var a = new ShapeObject(ColorResource.吾王蓝, new FCircle(40), 400, 300);
+			//replace with a file path in desk
+			var b = ImageObject.FromFile(@"C:\frice.png", 300, 400, 50, 50);
 //            var c = ImageObject.FromWeb("https://avatars1.githubusercontent.com/u/21008243", 400, 300);
 
-            //can resize：
-            a.Height = 100;
-            a.Width = 100;
+			//can resize：
+			a.Height = 100;
+			a.Width = 100;
 //            c.Height = 100;
 //            c.Width = 100;
-            a.MoveList.Add(new SimpleMove(10, 10));
-            b.MoveList.Add(new SimpleMove(10, -10));
+			a.MoveList.Add(new SimpleMove(10, 10));
+			b.MoveList.Add(new SimpleMove(10, -10));
 //            c.MoveList.Add(new SimpleMove(-10, 10));
-            AddObject(a);
-            AddObject(b);
+			AddObject(a);
+			AddObject(b);
 //            AddObject(c);
-        }
-    }
+		}
+	}
 }
