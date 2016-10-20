4be48fa91ecb1c516315034c7ae353e17c472856
ice1000 <ice1000kotlin@foxmail.com>
2016-10-20 14:12:55 +0800

    add FileUtils

---
+++ FriceEngine/Utils/File/FileUtils.cs
@@ -0,0 +1,22 @@
+﻿using System.IO;
+
+namespace FriceEngine.Utils.File
+{
+	public static class FileUtils
+	{
+		public static void String2File(string path, string content)
+		{
+			var s = new FileInfo(path).CreateText();
+			s.WriteLine(content);
+			s.Close();
+		}
+
+		public static string File2String(string path)
+		{
+			var s = new FileInfo(path).OpenText();
+			var ret = s.ReadToEnd();
+			s.Close();
+			return ret;
+		}
+	}
+}
